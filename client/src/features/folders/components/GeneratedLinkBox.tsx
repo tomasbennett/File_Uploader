14bb0cb2d26e693a2ddbc94e8f0838d62a946976
tomasbennett <tjs.bennett@gmail.com>
2025-12-22 00:43:28 +0000

    New GeneratedLinkBox component for folders with links present

---
+++ client/src/features/folders/components/GeneratedLinkBox.tsx
@@ -0,0 +1,26 @@
+import styles from "./GeneratedLinkBox.module.css";
+
+
+type IGeneratedLinkBoxProps = {
+    link: string;
+    header: string;
+    svgIcon?: React.ReactNode;
+};
+
+
+export function GeneratedLinkBox({ link, header, svgIcon }: IGeneratedLinkBoxProps) {
+    
+    
+    
+    
+    
+    
+    
+    return (
+        <>
+
+            
+        
+        </>
+    )
+}
